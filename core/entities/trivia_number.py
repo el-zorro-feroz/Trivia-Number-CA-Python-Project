ce7cc7372f20c9e56e5599962e057159876017a3
danwirele <najdisd2@mail.ru>
2022-10-15 16:36:35 +0300

    create entity and repository

--- core/entities/trivia_number.py
+++ core/entities/trivia_number.py
@@ -1,2 +1,6 @@
 # TriviaNumbers
 # UR JOB: Create correct entity (based on API Data)
+class TriviaNumber:
+    num: int
+    meaning: str
+
--- core/repositories/trivia_repository.py
+++ core/repositories/trivia_repository.py
@@ -1,2 +1,16 @@
 # TriviaRepository
 # UR JOB: Create Repository Interface for future Data Sources
+from abc import abstractmethod, ABC
+from requests import get
+from core.entities.trivia_number import TriviaNumber
+
+
+class TriviaRepository:
+
+    @abstractmethod
+    def get_concrete_num_meaning(self) -> TriviaNumber:
+        return NotImplemented
+
+    @abstractmethod
+    def get_random_num_meaning(self) -> TriviaNumber:
+        return NotImplemented
