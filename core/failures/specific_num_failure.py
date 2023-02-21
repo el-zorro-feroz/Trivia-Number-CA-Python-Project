b8213d69b3beabef118ebeb3f5400166cad4d728
Pocket-Red-Fox <90259229+pocket-red-fox@users.noreply.github.com>
2023-02-21 19:44:24 +0300

    update: failure description

--- core/failures/specific_num_failure.py
+++ core/failures/specific_num_failure.py
@@ -2,8 +2,8 @@ from core.failure import Failure
 
 
 class NotEnteredFailure(Failure):
-    error_message = "Хорошее число не введено, давай по новой"
+    error_message = "The number is not entered"
 
 
 class LowerThenNullFailure(Failure):
-    error_message = "Миша, всё плохо, давай по новой, число меньше 0"
+    error_message = "The specified number is less than 0"
